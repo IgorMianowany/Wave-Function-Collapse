0f692a0e317a13e21800850230aedb9805bfbce4
Igor Mianowany <fimiany@gmail.com>
2022-08-29 21:36:26 +0200

    Working wave function collapse with reset button

--- src/main/java/tiles/TileManager.java
+++ src/main/java/tiles/TileManager.java
@@ -1,9 +1,11 @@
 package tiles;
 
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class TileManager {
    private final Tile[][] tiles = new Tile[5][5];
@@ -38,17 +40,79 @@ public class TileManager {
         for(int i = 0; i<5;i++){
             for(int j = 0; j<5; j++){
                 if(j>0){
-                    if(tiles[j-1][i].getRightConnector() == 1){
+                    if(i==0){
+                        if(tiles[j-1][i].getRightConnector() == 1){
+                            originalOptions = tiles[j][i].getOriginalOptions();
+                            optionsToRemove.add(originalOptions.get(0));
+                            optionsToRemove.add(originalOptions.get(1));
+                            optionsToRemove.add(originalOptions.get(2));
+                            tiles[j][i].limitOptions(optionsToRemove);
+                            tiles[j][i].collapse();
+                            optionsToRemove.clear();
+                        }
+                    }
+                    else{
                         originalOptions = tiles[j][i].getOriginalOptions();
-                        optionsToRemove.add(originalOptions.get(0));
-                        optionsToRemove.add(originalOptions.get(2));
-                        optionsToRemove.add(originalOptions.get(3));
-                        tiles[j][i].limitOptions(optionsToRemove);
-                        tiles[j][i].collapse();
-                        optionsToRemove.clear();
+                        if(tiles[j-1][i].getRightConnector() == 0 && tiles[j][i-1].getDownConnector() == 0){
+                            optionsToRemove.add(originalOptions.get(0));
+                            optionsToRemove.add(originalOptions.get(1));
+                            optionsToRemove.add(originalOptions.get(3));
+                            optionsToRemove.add(originalOptions.get(4));
+                            tiles[j][i].limitOptions(optionsToRemove);
+                            tiles[j][i].collapse();
+                            optionsToRemove.clear();
+                        }
+                        else if(tiles[j-1][i].getRightConnector() == 1 && tiles[j][i-1].getDownConnector() == 0){
+                            optionsToRemove.add(originalOptions.get(0));
+                            optionsToRemove.add(originalOptions.get(1));
+                            optionsToRemove.add(originalOptions.get(2));
+                            tiles[j][i].limitOptions(optionsToRemove);
+                            tiles[j][i].collapse();
+                            optionsToRemove.clear();
+                        }
+                        else if(tiles[j-1][i].getRightConnector() == 1 && tiles[j][i-1].getDownConnector() == 1){
+                            optionsToRemove.add(originalOptions.get(0));
+                            optionsToRemove.add(originalOptions.get(2));
+                            optionsToRemove.add(originalOptions.get(3));
+                            optionsToRemove.add(originalOptions.get(4));
+                            tiles[j][i].limitOptions(optionsToRemove);
+                            tiles[j][i].collapse();
+                            optionsToRemove.clear();
+                        }
+                        else if(tiles[j-1][i].getRightConnector() == 0 && tiles[j][i-1].getDownConnector() == 1){
+                            optionsToRemove.add(originalOptions.get(1));
+                            optionsToRemove.add(originalOptions.get(2));
+                            optionsToRemove.add(originalOptions.get(3));
+                            optionsToRemove.add(originalOptions.get(4));
+                            tiles[j][i].limitOptions(optionsToRemove);
+                            tiles[j][i].collapse();
+                            optionsToRemove.clear();
+                        }
                     }
                 }
+                else{
+                    if (i > 0){
+                        if(tiles[j][i-1].getDownConnector() == 1){
+                            originalOptions = tiles[j][i].getOriginalOptions();
+                            optionsToRemove.add(originalOptions.get(1));
+                            optionsToRemove.add(originalOptions.get(2));
+                            optionsToRemove.add(originalOptions.get(3));
+                            optionsToRemove.add(originalOptions.get(4));
+                            tiles[j][i].limitOptions(optionsToRemove);
+                            tiles[j][i].collapse();
+                            optionsToRemove.clear();
+                        }
+                        if(tiles[j][i-1].getDownConnector() == 0){
+                            originalOptions = tiles[j][i].getOriginalOptions();
+                            optionsToRemove.add(originalOptions.get(0));
+                            optionsToRemove.add(originalOptions.get(1));
+                            tiles[j][i].limitOptions(optionsToRemove);
+                            tiles[j][i].collapse();
+                            optionsToRemove.clear();
+                        }
+                    }
 
+                }
             }
         }
     }
--- src/main/java/wfc/GUI.java
+++ src/main/java/wfc/GUI.java
@@ -2,17 +2,14 @@ package wfc;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
-import tiles.Tile;
-import tiles.TileFactory;
+
 import tiles.TileManager;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class GUI extends Application {
 
@@ -20,15 +17,29 @@ public class GUI extends Application {
     public void start(Stage stage) throws IOException {
         GridPane grid = new GridPane();
         TileManager tileManager = new TileManager();
-
-
-
-
         for(int i = 0; i<5; i++){
             for(int j = 0; j<4; j++){
                 grid.add(tileManager.getTiles()[i][j].getChoice(),i,j);
             }
         }
+        Button button = new Button();
+        button.setText("Reset");
+        button.setTranslateY(-90);
+        grid.add(button,0,0);
+
+        button.setOnAction(actionEvent -> {
+            TileManager tileManager2 = new TileManager();
+            grid.getChildren().clear();
+            for(int i = 0; i<5; i++){
+                for(int j = 0; j<4; j++){
+                    grid.add(tileManager2.getTiles()[i][j].getChoice(),i,j);
+                }
+            }
+            grid.add(button,0,0);
+        });
+
+
+
         Scene scene = new Scene(grid, 1000, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
